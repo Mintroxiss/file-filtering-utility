29fa3da9480640f5ce58308363029f866db7a17d
danilshkuratetskiy <shkuratetskiydanil@gmail.com>
2026-02-01 14:51:16 +0700

    Added README.md, and created task for building

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "ru.danilshkuratetskiy"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -14,6 +15,27 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
+application {
+    mainClass = "Main"
+}
+
+tasks.register("fatJar", Jar::class) {
+    group = "build"
+    description = "Собирает исполняемый jar с зависимостями"
+
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+
+    archiveBaseName.set(project.name)
+    archiveVersion.set(project.version.toString())
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.exists() }.map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
 }
